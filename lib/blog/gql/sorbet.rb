b92b003bfdea23be2165a4087f86c627323b445f
Guillaume Malette <gmalette@gmail.com>
2020-11-02 12:04:49 -0500

    GraphQL Schema and fields

--- lib/blog/gql/sorbet.rb
+++ lib/blog/gql/sorbet.rb
@@ -36,3 +36,36 @@ class Domain::Message
   attr_reader(:from_name)
 end
 
+# For this example, we'll be using mostly
+# the `MutationRoot`, but the graphql-ruby
+# gem requires a valid `QueryRoot`, so we'll
+# provide it with a dummy one.
+class Api::QueryRoot < GraphQL::Schema::Object
+  field(:version, Integer, null: false)
+end
+
+class Api::Message < GraphQL::Schema::Object
+  field(:content, String, null: false)
+  field(:from_name, String, null: true)
+end
+
+class Api::MessageInput < GraphQL::Schema::InputObject
+  extend(T::Sig)
+
+  argument(:content, String, required: true)
+  argument(:from_name, String, required: false)
+end
+
+class Api::MutationRoot < GraphQL::Schema::Object
+  extend(T::Sig)
+
+  field(:message_create, Api::Message, null: false) do |f|
+    f.argument(:message_input, Api::MessageInput, required: true)
+  end
+end
+
+class Api::Schema < GraphQL::Schema
+  query(Api::QueryRoot)
+  mutation(Api::MutationRoot)
+end
+
